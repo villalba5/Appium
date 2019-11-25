24c96e5f135f65552610d5e345b93b37d90c840a
villalba5 <44021441+villalba5@users.noreply.github.com>
2019-11-25 15:52:28 +0100

    add files

---
+++ TestAppiumAndroid/src/UIAutomatorTest.java
@@ -0,0 +1,22 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class UIAutomatorTest extends Base{
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		AndroidDriver<AndroidElement> driver = Capabilities();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		//en selenium no tenemos estos selectores 
+		// driver.findelementByAndroidUIAutomator("atribute("value")")
+		
+		driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
+		driver.findElementByAndroidUIAutomator("text(\"Animation\")").click();
+	}
+
+}
